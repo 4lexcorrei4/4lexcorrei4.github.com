182cff0712912cc056521db890ec612e54b75603
4lexcorrei4 <at.correia@campus.fct.unl.pt>
2019-12-24 13:25:25 +0000

    Optimized code

--- js/script.js
+++ js/script.js
@@ -1,66 +1,40 @@
-/* MENU */
-document.getElementById('mobile-menu-open').click(function() {
-    document.getElementById('menu').toggle(250);
-    document.getElementById('menu').css('display', 'block');
-    document.getElementById('mobile-menu-open').css('display', 'none');
-    document.getElementById('mobile-menu-close').css('display', 'block');
-});
+/* -------- MENU -------- */
 
-document.getElementById('mobile-menu-close').click(function() {
-    document.getElementById('menu').css('display', 'none');
-    document.getElementById('mobile-menu-open').css('display', 'block');
-    document.getElementById('mobile-menu-close').css('display', 'none');
-});
-
-/*document.getElementById('page-beggining').waypoint(function(direction) {
-    if (direction == 'down') {
-        document.getElementById('menu').style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-    } else {
-        document.getElementById('menu').style.backgroundColor = 'rgba(255, 255, 255, 0)';
-    }
-    /*if (direction == "down" && ($('.mobile-menu-open').css('display') == 'none' && $('.mobile-menu-close').css('display') == 'none')) {
-        $('.sticky-menu').css('display', 'block');
-    } else if ($('.mobile-menu-open').css('display') == 'none' && $('.mobile-menu-close').css('display') == 'none') {
-        $('.sticky-menu').css('display', 'none');
-    }*/
-    /*if (direction == "down") {
-        $('.menu').css('background-color', 'rgba(255, 255, 255, 0.9)');
-    } else {
-        $('.menu').css('background-color', 'rgba(255, 255, 255, 0)');
-    }*/
-/*}, {
-  offset: '60px;'
-});*/
-
-/* Navigation scroll */
-/*$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
+function showHideMenu() {
+    if (window.matchMedia('(max-device-width: 480px)').matches) {
+        if ($('#sidebar').css("display") == "none") {
+            $('#mobile-menu-open').css("display", "none");
+            $('#mobile-menu-close').css("display", "block");
+        } else {
+            $('#mobile-menu-open').css("display", "block");
+            $('#mobile-menu-close').css("display", "none");
         }
-    });
-});*/
+        $('#sidebar').slideToggle();
+    }
+}
 
+function showHideSubmenu(element) {
+    if ($("#submenu-" + element).css("display") == "none")
+        $("#arrow-" + element).css("transform", "rotate(90deg)");
+    else
+        $("#arrow-" + element).css("transform", "rotate(0)");
+    $("#submenu-" + element).slideToggle();
+}
 
-/* Mobile navigation */
-/*$('.js--nav-icon').click(function() {
-    var nav = $('.js--main-nav');
-    var icon = $('.js--nav-icon i');
+/* -------- COURSES -------- */
 
-    nav.slideToggle(200);
+function showHideCourses(type) {
+    var types = ["courses-online-contents", "courses-summer-contents"];
+    for (var thisType = 0; thisType < types.length; thisType++) {
+        if (types[thisType] == type)
+            $("#" + types[thisType]).slideToggle();
+        else if ($("#" + types[thisType]).css("display") == "block")
+            $("#" + types[thisType]).slideToggle();
+    }
+}
 
-    if (icon.hasClass('ion-navicon-round')) {
-        icon.addClass('ion-close-round');
-        icon.removeClass('ion-navicon-round');
-    } else {
-        icon.addClass('ion-navicon-round');
-        icon.removeClass('ion-close-round');
-    }        
-});*/
+/* -------- DETAILS -------- */
+
+function showHideDetails(block) {
+    $("#" + block + "-details").slideToggle();
+}
