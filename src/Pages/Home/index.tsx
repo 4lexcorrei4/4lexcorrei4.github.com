8ff445c9daa3f17c50600ccfe87175bad04359d0
Alexandre Correia <at.correia@campus.fct.unl.pt>
2022-10-25 23:13:16 +0100

    Updated welcome page & Updated hobbies

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -4,13 +4,13 @@ import {IoLogoFacebook, IoLogoGithub, IoLogoInstagram, IoLogoLinkedin} from "rea
 
 const Home = () => {
     return <div id="home" className="page">
-        <div id="welcome">Hey, I am</div>
+        <div id="welcome">Hello, I am</div>
         <div id="name">Alexandre Correia</div>
         <div id="social">
             <a href={"https://www.linkedin.com/in/4lexcorrei4/"} target="_blank" rel="noopener noreferrer"><IoLogoLinkedin /></a>
-            <a href={"https://www.facebook.com/4lexcorrei4/"} target="_blank" rel="noopener noreferrer"><IoLogoFacebook /></a>
             <a href={"https://www.instagram.com/4lexcorrei4/"} target="_blank" rel="noopener noreferrer"><IoLogoInstagram /></a>
             <a href={"https://github.com/4lexcorrei4/"} target="_blank" rel="noopener noreferrer"><IoLogoGithub /></a>
+            <a href={"https://www.facebook.com/4lexcorrei4/"} target="_blank" rel="noopener noreferrer"><IoLogoFacebook /></a>
         </div>
     </div>
 };
--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -14,10 +14,10 @@ const Profile = () => {
             <li><img src="/img/mobile_applications.svg" />Mobile Applications</li>
             <li><img src="/img/machine_learning.svg" />Machine Learning</li>
         </ul>
-        <h2>Main Hobbies</h2>
+        <h2>Hobbies</h2>
         <ul className="cols-2">
             <li><img src="/img/hicking.svg" />Hiking</li>
-            <li><img src="/img/sailing.svg" />Sailing</li>
+            <li><img src="/img/sports.svg" />Sports</li>
         </ul>
     </div>
 };
