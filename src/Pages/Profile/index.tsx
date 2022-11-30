cdbb8accb46da50cbeda46365d661de7fc53fca9
Alexandre Correia <at.correia@campus.fct.unl.pt>
2022-11-30 23:31:16 +0000

    Update description and hobbies

--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./style.css"
 import {externalUrls, internalUrls} from "../../Infos/urls";
-import {Link} from "react-router-dom";
 
 const Profile = () => {
     return <div id="profile" className="page bgcolor">
         <img id="profile-picture" src="/img/profile.png" />
-        <p>I am a Software Engineer at <a href={externalUrls.currentCompany()} target="_blank" rel="noopener noreferrer">Caixa Mágica Software</a>, where I am doing full-stack web development.</p>
+        <p>I am a Software Engineer at <a href={externalUrls.currentCompany()} target="_blank" rel="noopener noreferrer">Caixa Mágica Software</a>, where I am doing web development.</p>
         <p>I have always been very passionate about computers and lately the area that I have been focusing the most is the development of Web Applications, which includes (distributed) back-end, front-end, database management and information extraction (e.g. through crawling). Also, I enjoy incorporating some machine learning in the projects I develop, whenever needed.</p>
         <h2>Interests</h2>
         <ul className="cols-4">
@@ -16,9 +15,10 @@ const Profile = () => {
             <li><img src="/img/distributed_systems.svg" />Distributed Systems</li>
         </ul>
         <h2>Hobbies</h2>
-        <ul className="cols-2">
+        <ul className="cols-3">
             <li><img src="/img/hicking.svg" />Hiking</li>
-            <li><img src="/img/sports.svg" />Sports</li>
+            <li><img src="/img/sailing.svg" />Sailing</li>
+            <li><img src="/img/music.svg" />Music</li>
         </ul>
     </div>
 };
